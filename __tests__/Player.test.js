9b2e8d136fad2c60dfca1f1e23e7b835ab1412d9
cliffybar <crowellclifford@utexas.edu>
2021-04-24 03:35:47 -0500

    added methods to Player() constructor using prototype

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,16 +1,34 @@
-const Potion  = require('../lib/Potion');
-jest.mock('../lib/Potion');
-console.log(new Potion());
+const Player = require('../lib/Player.js');
+const Potion = require('../lib/Potion.js');
 
-const Player = require('../lib/Player');
+jest.mock('../lib/Potion.js');
 
 test('creates a player object', () => {
+  const player = new Player('Dave');
 
-    // when we run npm run test Player, it'll create 4 properties: player name, and the 3 player values
-    const player = new Player('Dave');
-  
-    expect(player.name).toBe('Dave');
-    expect(player.health).toEqual(expect.any(Number));
-    expect(player.strength).toEqual(expect.any(Number));
-    expect(player.agility).toEqual(expect.any(Number));
-  });
+  expect(player.name).toBe('Dave');
+  expect(player.health).toEqual(expect.any(Number));
+  expect(player.strength).toEqual(expect.any(Number));
+  expect(player.agility).toEqual(expect.any(Number));
+
+  expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
+});
+
+test("gets player's stats as an object", () => {
+  const player = new Player('Dave');
+
+  expect(player.getStats()).toHaveProperty('potions');
+  expect(player.getStats()).toHaveProperty('health');
+  expect(player.getStats()).toHaveProperty('strength');
+  expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+  const player = new Player('Dave');
+
+  expect(player.getInventory()).toEqual(expect.any(Array));
+
+  player.inventory = [];
+
+  expect(player.getInventory()).toEqual(false);
+});
